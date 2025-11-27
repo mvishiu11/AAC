7b99878ab217c4f16141bbadff9f4d3cd9dd97bf
RyuGracie <iwanikremek@gmail.com>
2025-11-27 17:06:20 +0100

    Change program call arguments and add docs

---
+++ implementation/README.md
@@ -0,0 +1,28 @@
+# Multigraph Subgraph Matching & Minimal Extension Implementation
+
+Calling form:
+```bash
+./main.o <exact/approx> <path-to-file> <N>
+```
+
+The file should be in form of:
+```
+V(G)
+[Adjancency matrix V(G)xV(G)]
+V(H)
+[Adjancency matrix V(H)xV(H)]
+```
+Example:
+```
+3
+4 2 5
+3 4 1
+2 3 4
+5
+4 2 5 3 1
+3 4 1 2 4
+2 3 4 5 0
+1 0 3 4 2
+5 1 0 2 3
+```
+Where column index represents source of edge, and row index represents destination of edge.
--- implementation/src/main.cpp
+++ implementation/src/main.cpp
@@ -8,58 +8,90 @@
 
 using namespace std;
 
-Graph* parseInput(string filename, int* N);
-Graph createGraphFromFile(ifstream& file);
+Graph *parseInput(string filename);
+Graph createGraphFromFile(ifstream &file);
 
-int maxK(int h, int g) {
-    // (h choose g)g! 
-    // = h!/(h-g)! 
+int maxK(int h, int g)
+{
+    // (h choose g)g!
+    // = h!/(h-g)!
     // = h*(h-1)*...*(h-g+2)*(h-g+1)
-    if (g > h) return 0;
-    if (g == 0) return 1;
+    if (g > h)
+        return 0;
+    if (g == 0)
+        return 1;
     int result = 1;
-    for (int i = 0; i < g; ++i) {
+    for (int i = 0; i < g; ++i)
+    {
         result *= (h - i);
     }
     return result;
 }
 
-int main(int argc, char** argv) {
-    if(argc < 2) return -1;
-    string filename = argv[1];
-    int N;
-    auto graphs = parseInput(filename, &N);
+int main(int argc, char **argv)
+{
+    if (argc < 4)
+        return -1;
+    string algorithm = argv[1];
+    string filename = argv[2];
+    int N = stoi(argv[3]);
+    if(algorithm!="exact" && algorithm!="approx"){
+        cout<<"Wrong algorithm option provided!"<<endl;
+        cout<<algorithm<<endl;
+        return -1;
+    }
+    if(N<1){
+        cout<<"N needa to be >1!"<<endl;
+        return -1;
+    }
+    auto graphs = parseInput(filename);
     Graph G = graphs[0];
     Graph H = graphs[1];
 
-    int mK = maxK(H.getVerticesCount(),G.getVerticesCount());
-    int K = 123;
-    K = std::min(K,mK); // In case our K is greater than maximum number of possible mappings
-    
-    cout << "### SELECT MAPPINGS ###" << endl;
-    const std::vector<Mapping> mappings = H.selectMappings(G,K);
+    int mK = maxK(H.getVerticesCount(), G.getVerticesCount());
+    int K = H.getVerticesCount()*G.getVerticesCount();
+    K = std::min(K, mK); // In case our K is greater than maximum number of possible mappings
 
-    if(H.hasNSubgraphs(G, N)) {
-        cout << "EXACT: YES" << endl;
-    } else {
-        cout << "EXACT: NO" << endl;
+    cout << "### SELECT MAPPINGS ###" << endl;
+    const std::vector<Mapping> mappings = H.selectMappings(G, K);
+
+    if (algorithm == "exact")
+    {
+        cout<<"\n ===== EXACT ALGORITHMS ===== \n"<<endl;
+        if (H.hasNSubgraphs(G, N))
+        {
+            cout << "EXACT: YES" << endl;
+        }
+        else
+        {
+            cout << "EXACT: NO" << endl;
+            H.findMinimalExtension(G, N);
+        }
     }
-    
-    if(H.hasNSubgraphsApprox(G, K, mappings, N)) {
-        cout << "APPROXIMATION: YES" << endl;
-    } else {
-        cout << "APPROXIMATION: NO" << endl;
+
+    if (algorithm == "approx")
+    {
+        cout<<"\n ===== APPROXIMATE ALGORITHMS ===== \n"<<endl;
+        if (H.hasNSubgraphsApprox(G, K, mappings, N))
+        {
+            cout << "APPROXIMATION: YES" << endl;
+        }
+        else
+        {
+            cout << "APPROXIMATION: NO" << endl;
+            H.findMinimalExtensionApprox(G, K, mappings, N);
+        }
     }
-    
-    H.findMinimalExtension(G, N);
-    H.findMinimalExtensionApprox(G, K, mappings, N);
+
     return 0;
 }
 
-Graph* parseInput(string filename, int* N) {
-    ifstream file(filename); 
+Graph *parseInput(string filename)
+{
+    ifstream file(filename);
 
-    if (!file) { 
+    if (!file)
+    {
         cerr << "Error opening file!" << endl;
         return nullptr;
     }
@@ -67,31 +99,31 @@ Graph* parseInput(string filename, int* N) {
     Graph G = createGraphFromFile(file);
     Graph H = createGraphFromFile(file);
 
-    Graph* graphs = (Graph*)malloc(2 * sizeof(Graph));
+    Graph *graphs = (Graph *)malloc(2 * sizeof(Graph));
     graphs[0] = G;
     graphs[1] = H;
     string line;
-    getline(file, line);
-    *N = stoi(line);
     return graphs;
-}   
+}
 
-Graph createGraphFromFile(ifstream& file) {
+Graph createGraphFromFile(ifstream &file)
+{
     string line;
     getline(file, line);
     int nodes = stoi(line);
-    int** edges = new int*[nodes];
-    for(int i = 0; i < nodes; i++) {
+    int **edges = new int *[nodes];
+    for (int i = 0; i < nodes; i++)
+    {
         getline(file, line);
         istringstream iss(line);
         edges[i] = new int[nodes];
         string word;
         int j = 0;
-        while(iss>>word) {
+        while (iss >> word)
+        {
             edges[i][j++] = stoi(word);
         }
     }
     Graph graph(nodes, edges);
     return graph;
 }
-
